21a88da2bf0de2635300cbd9502896a8d2692760
BivekKumarMali <bivekkumar@promactinfo.com>
2020-11-22 11:54:11 +0530

    Intial Commit

---
+++ aspnet-core/src/EmployeeManagement.EntityFrameworkCore/EntityFrameworkCore/EmployeeManagementDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Abp.Zero.EntityFrameworkCore;
+using EmployeeManagement.Authorization.Roles;
+using EmployeeManagement.Authorization.Users;
+using EmployeeManagement.MultiTenancy;
+
+namespace EmployeeManagement.EntityFrameworkCore
+{
+    public class EmployeeManagementDbContext : AbpZeroDbContext<Tenant, Role, User, EmployeeManagementDbContext>
+    {
+        /* Define a DbSet for each entity of the application */
+        
+        public EmployeeManagementDbContext(DbContextOptions<EmployeeManagementDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
